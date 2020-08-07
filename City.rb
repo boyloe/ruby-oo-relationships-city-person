9f68eb8eaf23e14f4c0a6eebb09d49f5b79121c4
boyloe <boyloe@gmail.com>
2020-08-07 15:13:50 -0600

    Include citizens method to City class

--- City.rb
+++ City.rb
@@ -1,7 +1,17 @@
 class City
   attr_reader :name
-
+ 
   def initialize name
     @name = name
+  end
+
+  def add_person person 
+    person.city = self
+  end
+
+  def citizens
+    Person.all.select do |person|
+      person.city == self
+    end
   end
 end
--- Person.rb
+++ Person.rb
@@ -1,9 +1,15 @@
 class Person
   attr_reader :first_name, :last_name
   attr_accessor :city
-
+   @@all = []
   def initialize first_name, last_name
     @first_name = first_name
     @last_name = last_name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+  
 end
